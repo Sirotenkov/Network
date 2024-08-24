35699d86ecb19874c990920845cd16f507535c85
Алексей <aleksej.sirotenkov@simaster.ru>
2024-08-24 15:50:00 +0300

    Добавлена реализация класса viewmodel

--- Network/viewmodel.cpp
+++ Network/viewmodel.cpp
@@ -1,6 +1,56 @@
+#include "model.h"
 #include "viewmodel.h"
 
-ViewModel::ViewModel(QObject *parent) : QObject(parent)
+ViewModel::ViewModel(Model* model, QObject *parent) : QObject(parent), m_model(model)
 {
+    connect(m_model, &Model::currentweatherUpdated, this, &ViewModel::currentweatherUpdated);
+    connect(&m_timer, &QTimer::timeout, this, &ViewModel::onTimeout);
+    m_timer.start(m_interval);
 
+    onTimeout();
+}
+
+QDateTime ViewModel::currentDateTime() const
+{
+    auto const currentweather = m_model->currentweather();
+    return !currentweather.isEmpty() ? currentweather.front().currentDateTime
+                : QDateTime::currentDateTimeUtc();
+}
+
+double ViewModel::temp() const
+{
+    auto const currentweather = m_model->currentweather();
+    return !currentweather.isEmpty() ? currentweather.front().temp : 0.0;
+}
+
+double ViewModel::windSpeed() const
+{
+    auto const currentweather = m_model->currentweather();
+    return !currentweather.isEmpty() ? currentweather.front().windSpeed : 0.0;
+}
+
+int ViewModel::humidity() const
+{
+    auto const currentweather = m_model->currentweather();
+    return !currentweather.isEmpty() ? currentweather.front().humidity: 0;
+}
+
+QString ViewModel::tempUnits() const
+{
+    return m_model->tempUnits();
+}
+
+QString ViewModel::windSpeedUnits() const
+{
+    return m_model->windSpeedUnits();
+}
+
+QString ViewModel::humidityUnits() const
+{
+    return m_model->humidityUnits();
+}
+
+void ViewModel::onTimeout()
+{
+    m_model->updateCurrentweather();
 }
--- Network/viewmodel.h
+++ Network/viewmodel.h
@@ -2,16 +2,54 @@
 #define VIEWMODEL_H
 
 #include <QObject>
+#include <QDateTime>
+#include <QTimer>
+
+class Model;
 
 class ViewModel : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QDateTime currentDateTime READ currentDateTime NOTIFY currentweatherUpdated)
+
+    Q_PROPERTY(double temp READ temp NOTIFY currentweatherUpdated)
+    Q_PROPERTY(double windSpeed READ windSpeed NOTIFY currentweatherUpdated)
+    Q_PROPERTY(int humidity READ humidity NOTIFY currentweatherUpdated)
+
+    Q_PROPERTY(QString tempUnits READ tempUnits NOTIFY currentweatherUpdated)
+    Q_PROPERTY(QString windSpeedUnits READ windSpeedUnits NOTIFY currentweatherUpdated)
+    Q_PROPERTY(QString humidityUnits READ humidityUnits NOTIFY currentweatherUpdated)
+
 public:
-    explicit ViewModel(QObject *parent = nullptr);
+    explicit ViewModel(Model* model, QObject *parent = nullptr);
+
+    QDateTime currentDateTime() const;
+
+    double temp() const;
+    double windSpeed() const;
+    int humidity() const;
+
+    QString tempUnits() const;
+    QString windSpeedUnits() const;
+    QString humidityUnits() const;
 
 signals:
+    void currentweatherUpdated();
 
+private slots:
+    void onTimeout();
+
+private:
+    Model* m_model = nullptr;
+    QTimer m_timer;
+
+    // INFO: Интервал обновления данных с сайта
+    int m_interval = 1 * 60 * 1000;
+
+    QString m_tempUnits;
+    QString m_windSpeedUnits;
+    QString m_humidityUnits;
 };
 
 #endif // VIEWMODEL_H
